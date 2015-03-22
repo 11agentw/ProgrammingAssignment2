76dcacec0562bc9d3856f41bcfca3c605af5e658
11agentw <11agentw@gmail.com>
2015-03-22 14:37:52 -0400

    Full submittal for HW #2
    makeCacheMatrix and cacheSolve functions completed and commented

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions allow for the inverse of a matrix to be cached in order 
+## to avoid repeating the time-consuming calculation.
 
-## Write a short comment describing this function
+## 'makeCacheMatrix' creates a list containing four functions:
+##    set - set the value of a matrix
+##    get - return the matrix value
+##    setinverse - set the inverse of the stored matrix
+##    getinverse - return the stored inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- cacheSolve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## 'cacheSolve' is used in conjunction with 'makeCacheMatrix$setinverse' in
+## order to calculate a matrix that is the inverse of 'x'. Before trying to
+## solve for the matrix, the function checks to see if the solution is already
+## cached and need not be calculated again.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  
+  data <- x$get()
+  
+  ## Error checks for a non-square matrix and the determinant of the matrix
+  ## being equal to zero.
+  
+  if(nrow(data) != ncol(data)) {
+    stop("The inverse of a non-square matrix cannot be calculated.")
+  }
+  if(det(data) == 0) {
+    stop("A matrix with a determinant of zero has no inverse.")
+  }
+  
+  ## Solve for the inverse of the matrix and set cache
+  
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
